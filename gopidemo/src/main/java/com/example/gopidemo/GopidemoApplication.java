7bec38e7c1fd2e7b61293832350f44c43c17ccfd
satyasri73 <satya.j@suntechnoservices.com>
2020-03-12 13:37:11 +0530

    this is gopi

---
+++ gopidemo/src/main/java/com/example/gopidemo/GopidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gopidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GopidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gopidemo/GopidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gopidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
